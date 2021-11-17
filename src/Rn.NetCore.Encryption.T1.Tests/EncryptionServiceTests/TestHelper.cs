db42d04dde5d682c049044fa7f8f196bef6dd4c6
Richard Niemand <niemand.richard@gmail.com>
2021-11-17 16:27:57 -0700

    Hit roadblock that needs to be addressed

---
+++ src/Rn.NetCore.Encryption.T1.Tests/EncryptionServiceTests/TestHelper.cs
@@ -0,0 +1,37 @@
+﻿using NSubstitute;
+using Rn.NetCore.Common.Configuration;
+using Rn.NetCore.Common.Logging;
+using Rn.NetCore.Encryption.T1.Tests.TestSupport;
+
+namespace Rn.NetCore.Encryption.T1.Tests.EncryptionServiceTests
+{
+  public static class TestHelper
+  {
+    public static EncryptionService GetService(
+      ILoggerAdapter<EncryptionService> logger = null,
+      IEncryptionHelper encryptionHelper = null,
+      ICommonConfigProvider configProvider = null,
+      EncryptionServiceConfig config = null)
+    {
+      return new EncryptionService(
+        logger ?? Substitute.For<ILoggerAdapter<EncryptionService>>(),
+        encryptionHelper ?? Substitute.For<IEncryptionHelper>(),
+        GetConfigProvider(configProvider, config)
+      );
+    }
+
+    public static ICommonConfigProvider GetConfigProvider(
+      ICommonConfigProvider configProvider = null,
+      EncryptionServiceConfig config = null)
+    {
+      if (configProvider != null)
+        return configProvider;
+
+      config ??= new EncryptionServiceConfigBuilder().BuildWithDefaults();
+      configProvider = Substitute.For<ICommonConfigProvider>();
+      configProvider.GetEncryptionServiceConfig().Returns(config);
+
+      return configProvider;
+    }
+  }
+}
---
+++ src/Rn.NetCore.Encryption.T1.Tests/TestSupport/Builders/EncryptionServiceConfigBuilder.cs
@@ -0,0 +1,68 @@
+﻿using Rn.NetCore.Encryption.Configuration;
+
+namespace Rn.NetCore.Encryption.T1.Tests.TestSupport
+{
+  public class EncryptionServiceConfigBuilder
+  {
+    private readonly EncryptionServiceConfig _config;
+
+    public EncryptionServiceConfigBuilder()
+    {
+      _config = new EncryptionServiceConfig();
+    }
+
+
+    public EncryptionServiceConfigBuilder WithDefaults()
+    {
+      _config.Enabled = true;
+      _config.Key = "rigeU7mR2zA=";
+      _config.IV = "5ffasfasg4w/stkaYXm/+Mi4Aw=";
+      _config.LogDecryptInput = false;
+      _config.LoggingEnabled = false;
+
+      return this;
+    }
+
+    public EncryptionServiceConfigBuilder WithDefaults(bool enabled)
+      => WithDefaults().WithEnabled(enabled);
+
+    public EncryptionServiceConfigBuilder WithKey(string key)
+    {
+      _config.Key = key;
+      return this;
+    }
+
+    public EncryptionServiceConfigBuilder WithIV(string iv)
+    {
+      _config.IV = iv;
+      return this;
+    }
+
+    public EncryptionServiceConfigBuilder WithLoggingEnabled(bool enabled)
+    {
+      _config.LoggingEnabled = enabled;
+      return this;
+    }
+
+    public EncryptionServiceConfigBuilder WithLogDecryptInput(bool enabled)
+    {
+      _config.LogDecryptInput = enabled;
+      return this;
+    }
+
+    public EncryptionServiceConfigBuilder WithEnabled(bool enabled)
+    {
+      _config.Enabled = enabled;
+      return this;
+    }
+
+
+    public EncryptionServiceConfig Build() => _config;
+
+    public EncryptionServiceConfig BuildWithDefaults()
+      => WithDefaults().Build();
+
+    public EncryptionServiceConfig BuildWithDefaults(bool enabled)
+      => WithDefaults(enabled).Build();
+  }
+}
